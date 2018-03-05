eb0c7408d5d0fbfb4914e62f3d317343ff28c455
Josep <josep.lagunas@gmail.com>
2018-03-05 21:39:47 +0100

    refactor

---
+++ Resolver/InstanceBuilder.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using Yuki.Core.Resolver.Infrastructure;
+
+namespace Yuki.Core.Resolver
+{
+    class InstanceBuilder : Singleton<InstanceBuilder>
+    {
+        private delegate dynamic CreateInstanceDelegate();
+        private IDictionary<Type, CreateInstanceDelegate> constructors;
+
+        private InstanceBuilder()
+        {
+            constructors = new Dictionary<Type, CreateInstanceDelegate>();
+        }
+
+        private static InstanceBuilder InitializeInstance()
+        {
+            return new InstanceBuilder();
+        }
+
+        public bool TryToCreateInstance<T>(Type outputType, out T instance)
+        {
+            if (!GetInstance().constructors.TryGetValue(outputType,
+                out CreateInstanceDelegate constructorDelegate))
+            {
+                lock (outputType)
+                {
+                    if (!GetInstance().constructors.TryGetValue(outputType,
+                        out constructorDelegate))
+                    {
+                        constructorDelegate = BuildConstructorDelegate(outputType);
+
+                        constructors.Add(outputType, constructorDelegate);
+                    }
+                }
+            }
+
+            instance = (T)constructorDelegate();
+
+            return instance != null;
+        }
+
+        private CreateInstanceDelegate BuildConstructorDelegate(Type outputType)
+        {
+            CreateInstanceDelegate constructorDelegate;
+            DynamicMethod dynamicMethod = new DynamicMethod("Constructor_" + outputType.Name
+                , outputType, new Type[0]);
+
+            ConstructorInfo constructorInfo = outputType.GetConstructor(new Type[0]);
+            ILGenerator iLGenerator = dynamicMethod.GetILGenerator();
+            iLGenerator.Emit(OpCodes.Newobj, constructorInfo);
+            iLGenerator.Emit(OpCodes.Ret);
+
+            constructorDelegate =
+                (CreateInstanceDelegate)dynamicMethod
+                .CreateDelegate(typeof(CreateInstanceDelegate));
+
+            return constructorDelegate;
+        }
+    }
+}
