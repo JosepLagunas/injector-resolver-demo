1e0d5c263d7442ec4ec087e0712b8db14a1ce5e2
Josep Lagunas <josep.lagunas@gmail.com>
2018-03-05 10:45:56 +0100

    Add project Yuki.Core.Implementation.User (Net Standard 2.0) (#4)

---
+++ Yuki.Core.Implementations.User/User/User.cs
@@ -0,0 +1,27 @@
+﻿using Yuki.Core.Interfaces.User;
+
+namespace Yuki.Core.Implementations.User.User
+{
+	class User : IUser
+	{
+		private string name;
+		public string Name { get => name; set => name = value; }
+
+		public static string StaticPropertyValueNotAccessibleViaInterface { get => "Static info."; }
+
+		public static int StaticMethodValueNotAccessibleViaInterface()
+		{
+			return 36;
+		}
+
+		public void Init()
+		{
+			Name = "No name set";
+		}
+
+		public string SayHello()
+		{
+			return string.Format("Hi! My name is {0}", Name);
+		}
+	}
+}
